2cde6c88836af55722654edae39653ea2fe8dd43
Kmaschta <kevin@marmelab.com>
2019-08-14 16:38:12 +0200

    Migration PasswordInput to TypeScript

--- packages/ra-ui-materialui/src/input/PasswordInput.tsx
+++ packages/ra-ui-materialui/src/input/PasswordInput.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, FunctionComponent } from 'react';
 import PropTypes from 'prop-types';
-import { translate as withTranslation } from 'ra-core';
+import { InputProps, useTranslate } from 'ra-core';
+import { TextFieldProps } from '@material-ui/core/TextField';
 
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
@@ -9,7 +10,15 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 import TextInput from './TextInput';
 
-const PasswordInput = ({ translate, initiallyVisible, ...rest }) => {
+interface Props extends InputProps<TextFieldProps> {
+    initiallyVisible?: boolean;
+}
+
+const PasswordInput: FunctionComponent<Props> = ({
+    initiallyVisible,
+    ...rest
+}) => {
+    const translate = useTranslate();
     const [visible, setVisible] = useState(initiallyVisible);
 
     return (
@@ -35,7 +44,6 @@ const PasswordInput = ({ translate, initiallyVisible, ...rest }) => {
 };
 
 PasswordInput.propTypes = {
-    translate: PropTypes.func.isRequired,
     initiallyVisible: PropTypes.bool,
 };
 
@@ -43,4 +51,4 @@ PasswordInput.defaultProps = {
     initiallyVisible: false,
 };
 
-export default withTranslation(PasswordInput);
+export default PasswordInput;
